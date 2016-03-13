195dbd19d0357908cb9173cd029cf0c04d14a55c
Joe Weber <wosephjeber@gmail.com>
2016-03-12 20:49:53 -0600

    Use Grunt instead of Jekyll's built-in sass processing
    Grunt provides more options and flexibility, such as using postcss

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-build-control');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
 
   grunt.initConfig({
 
@@ -17,7 +20,50 @@ module.exports = function(grunt) {
           branch: 'master'
         }
       }
-    }
+    },
+    
+    sass: {
+      default: {
+        options: {
+          style: 'compact',
+          sourcemap: 'none'
+        },
+        files: [{
+          expand: true,
+          cwd: '_sass',
+          src: ['*.scss'],
+          dest: '_site/css',
+          ext: '.css'
+        }]
+      }
+    },
+    
+    postcss: {
+      options: {
+        map: true,
+
+        processors: [
+          require('autoprefixer')({browsers: 'last 2 versions'}),
+        ]
+      },
+      default: {
+        src: '_site/css/*.css'
+      }
+    },
+    
+    watch: {
+      default: {
+        files: ['js/**/*.js', '_sass/*.scss'],
+        tasks: ['sass:default', 'postcss:default']
+      },
+      options: {
+        interrupt: false,
+        nospawn: true,
+        event: 'all',
+        interval: 1000,
+        debounceDelay: 1000
+      },
+    },
   });
 
   grunt.registerTask('deploy', [
